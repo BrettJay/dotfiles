d52b8ca1feccf4b4bf4e9c26e06653f0b81b1324
Brett Jones <hello@brettjones.me>
2022-09-12 21:01:17 +0800

    Add keymaps
    (Stolen from theprimeagen; leader + p for paste without losing the
    yanked text in your pasteboard)

---
+++ nvim/after/plugin/keymap/init.lua
@@ -0,0 +1,9 @@
+local Remap = require("brett.keymap")
+local nnoremap = Remap.nnoremap
+local vnoremap = Remap.vnoremap
+local inoremap = Remap.inoremap
+local xnoremap = Remap.xnoremap
+local nmap = Remap.nmap
+
+-- greatest remap ever
+xnoremap("<leader>p", "\"_dP")
