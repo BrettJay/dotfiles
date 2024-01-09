8a110a14e1ffc91aeb8c8f0dc14847654837aea1
Brett Jones <hello@brettjones.me>
2024-01-09 22:07:04 +0800

    feat(trouble): keybindings

--- nvim/lua/custom/plugins/trouble.lua
+++ nvim/lua/custom/plugins/trouble.lua
@@ -6,4 +6,35 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
   },
+  config = function()
+    require("trouble").setup()
+
+    vim.keymap.set("n", "<leader>tt", function()
+      require("trouble").toggle()
+    end)
+
+    vim.keymap.set("n", "<leader>tw", function()
+      require("trouble").toggle("workspace_diagnostics")
+    end)
+
+    vim.keymap.set("n", "<leader>td", function()
+      require("trouble").toggle("document_diagnostics")
+    end)
+
+    vim.keymap.set("n", "<leader>tq", function()
+      require("trouble").toggle("quickfix")
+    end)
+
+    vim.keymap.set("n", "<leader>tl", function()
+      require("trouble").toggle("loclist")
+    end)
+
+    vim.keymap.set("n", "[t", function()
+      require("trouble").next({ skip_groups = true, jump = true })
+    end)
+
+    vim.keymap.set("n", "]t", function()
+      require("trouble").previous({ skip_groups = true, jump = true })
+    end)
+  end
 }
